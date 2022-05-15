8447dc32d3452291b58173840c257b6a65c5bc61
ni-saimon <nafizimtiaz314@gmail.com>
2022-05-15 15:29:58 +0600

    final

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 
 PATH = r"C:\Users\nafiz\Downloads\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 driver.get("https://orteil.dashnet.org/cookieclicker/")
+
+driver.implicitly_wait(5)
+
+cookie = driver.find_element_by_id("bigCookie")
+cookie_count = driver.find_element_by_id("cookies")
+items = [driver.find_element_by_id("productPrice" + str(i)) for i in range(1, -1, -1)]
+
+actions = ActionChains(driver)
+actions.click(cookie)
+
+for i in range(5000):
+    actions.perform()
+
+
+
+
+
